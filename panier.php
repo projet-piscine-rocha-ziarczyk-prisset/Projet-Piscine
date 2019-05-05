f5a5a982039aa8f6a2e5abef7bbd2c979d2e0743
paulzck <36414479+paulzck@users.noreply.github.com>
2019-05-05 18:22:00 +0200

    Add files via upload

---
+++ panier.php
@@ -0,0 +1,31 @@
+<?php require 'header.php'; ?>
+
+ 		<header class="page-header header container-fluid">
+ 			<div class="overlay"></div>
+			<div class="description">
+ 				<h1>Votre Panier</h1>
+			</div>
+<div class="container features">
+	
+ 	<div class="row">
+ 		<div class="col-lg-1 col-md-4 col-sm-12">	
+ 			<img src="panier.png">
+ 		</div>
+
+ 		<div class="col-lg-7 col-md-4 col-sm-12">
+			 <h3 class="feature-title" align="center">Article dans votre panier :</h3>
+		
+ 		</div>
+
+	<div class="col-lg-4 col-md-4 col-sm-12">
+	 
+	 	<a href="paiement.php" class="bouton" align="center" >Passer au paiement.</a><br><br><br><br>
+	</div>
+	
+	</div>
+
+
+
+ 	</header>
+
+<?php require 'footer.php'; ?>
